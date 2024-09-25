9670830fa5bdb0623b93cc9faf6b11dab6384e1d
emmanuelmejia1516 <a.23308051281197@cbtis128.edu.mx>
2024-09-24 19:48:26 -0600

    Add files via upload

---
+++ consumo de combustible.py
@@ -0,0 +1,12 @@
+# Solicitar los kilómetros recorridos y litros consumidos
+kilometros = float(input("Ingresa los kilómetros recorridos: "))
+litros = float(input("Ingresa los litros de combustible consumidos: "))
+# Calcular el consumo por kilómetro
+consumo_por_km = litros / kilometros
+# Mostrar el consumo de combustible por kilómetro
+print(f"El consumo de combustible por kilómetro es: {consumo_por_km:.2f} litros/km")
+
+
+print("")
+print("Mejia Suarez Emmanuel Alexander:mi practica es un Programa que solicite al usuario ingresar la cantidad de kilómetros recorridos por una motocicleta y la cantidad de litros de combustible que consumió durante ese recorrido que luego muestra el consumo de combustible por kilómetro")
+print("")
